9af7d874b8899173c320906866e09a906acd6a43
NMAS Amaral <nmasamaral@MacBook-Air-de-NMAS.local>
2021-03-31 22:44:53 -0300

    Initial Project

---
+++ DateLib/DateLib.h
@@ -0,0 +1,18 @@
+//
+//  DateLib.h
+//  DateLib
+//
+//  Created by NMAS Amaral on 31/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DateLib.
+FOUNDATION_EXPORT double DateLibVersionNumber;
+
+//! Project version string for DateLib.
+FOUNDATION_EXPORT const unsigned char DateLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateLib/PublicHeader.h>
+
+
